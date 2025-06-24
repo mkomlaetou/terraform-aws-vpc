ca09c439fb289d6a8a854214aee99c4b6667600e
mkomlaetou <m.komlaetou@gmail.com>
2025-06-24 07:54:56 +0000

    update vpc_details output priv_rt

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-/**
- * Outputs details about the VPC resources created by the module.
- * Includes IDs for the VPC, internet gateway, NAT gateway/instance, subnets.
- */
-
 output "vpc_details" {
   value = {
     vpc_id          = aws_vpc.main.id
@@ -11,7 +6,7 @@ output "vpc_details" {
     nat_ip          = var.enable_nat_gateway == true || var.enable_nat_instance == true ? aws_eip.nat_ip[0].public_ip : "N/A"
     nat_instance_id = var.enable_nat_instance == true ? aws_instance.nat_ec2[0].id : "N/A"
     subnets_id      = { for k, y in aws_subnet.main : k => y.id }
-    priv_rt         = var.enable_nat_instance == true ? aws_route_table.priv_rt[0].id : "N/A"
-    pub_rt         = aws_route_table.pub_rt[0].id
+    priv_rt         = var.enable_nat_gateway == true || var.enable_nat_instance == true ? aws_route_table.priv_rt[0].id : "N/A"
+    pub_rt          = aws_route_table.pub_rt[0].id
   }
 }
